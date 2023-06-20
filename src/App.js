de0c3bdec185431500fdbb33b8f6971b05666dbd
Daniel <danielvillery@gmail.com>
2023-06-20 19:33:12 -0400

    basic search functionality, results displaying in console log. need to build image urls and display on page next

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
 import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+import he from "he";
 
 function App() {
-  return <div className='App'>Movie Databse!!!</div>;
+  return (
+    <div className='App'>
+      Movie Database!!!
+      <SearchBar />
+    </div>
+  );
+}
+
+function SearchBar() {
+  const [query, setQuery] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?query=${he.encode(query)}`,
+        {
+          headers: {
+            Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_TOKEN}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res.data.results);
+      });
+  };
+
+  return (
+    <>
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          className='search-bar'
+          type='text'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default App;
