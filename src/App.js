97753515b7c36ac0d2fea08d2836cdd751ab07f8
Daniel <danielvillery@gmail.com>
2023-06-21 01:58:43 -0400

    results displaying with images. need to style results

--- src/App.js
+++ src/App.js
@@ -4,15 +4,38 @@ import { useState } from "react";
 import he from "he";
 
 function App() {
+  const [results, setResults] = useState(null);
   return (
     <div className='App'>
       Movie Database!!!
-      <SearchBar />
+      <SearchBar setResults={setResults} />
+      <br />
+      {results &&
+        (results.length > 0 ? (
+          results.map((r) => (
+            <>
+              <div key={r.id}>{r.original_title}</div>
+              <img
+                key={r.backdrop_path}
+                src={
+                  r.backdrop_path == null
+                    ? "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png"
+                    : `https://image.tmdb.org/t/p/w500/${r.backdrop_path}`
+                }
+                alt=''
+              />
+            </>
+          ))
+        ) : (
+          <>
+            <div>No results found</div>
+          </>
+        ))}
     </div>
   );
 }
 
-function SearchBar() {
+function SearchBar({ setResults }) {
   const [query, setQuery] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,7 +43,7 @@ function SearchBar() {
       .get(
         `${process.env.REACT_APP_TMDB_URL}/search/movie?query=${he.encode(
           query
-        )}`,
+        )}?language=en-US`,
         {
           headers: {
             Authorization: `Bearer ${process.env.REACT_APP_TMDB_API_TOKEN}`,
@@ -29,6 +52,7 @@ function SearchBar() {
       )
       .then((res) => {
         console.log(res.data.results);
+        setResults(res.data.results);
       });
   };
 
