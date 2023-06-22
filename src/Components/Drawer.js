60edd3dd5a7939628ad95501049321ccf0a36235
Daniel <danielvillery@gmail.com>
2023-06-22 16:35:34 -0400

    mobile app drawer added media queries

--- src/Components/Drawer.js
+++ src/Components/Drawer.js
@@ -8,16 +8,43 @@ import {
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export function DrawerMenu() {
   const [open, setOpen] = useState(false);
+  const navigate = useNavigate();
   return (
     <>
       <Drawer open={open} onClose={() => setOpen(false)}>
         <List>
-          <ListItemButton>
+          <ListItemButton
+            onClick={() => {
+              navigate("/movies");
+              setOpen(!open);
+            }}
+          >
             <ListItemIcon>
-              <ListItem></ListItem>
+              <ListItem>Movies</ListItem>
+            </ListItemIcon>
+          </ListItemButton>
+          <ListItemButton
+            onClick={() => {
+              navigate("/tv");
+              setOpen(!open);
+            }}
+          >
+            <ListItemIcon>
+              <ListItem>TV Shows</ListItem>
+            </ListItemIcon>
+          </ListItemButton>
+          <ListItemButton
+            onClick={() => {
+              navigate("/people");
+              setOpen(!open);
+            }}
+          >
+            <ListItemIcon>
+              <ListItem>People</ListItem>
             </ListItemIcon>
           </ListItemButton>
         </List>
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -2,25 +2,39 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 
-import { Tab, Tabs } from "@mui/material";
+import { Tab, Tabs, useMediaQuery, useTheme } from "@mui/material";
 import { SearchBar } from "./searchBar";
 import { useState } from "react";
+import { DrawerMenu } from "./Drawer";
+import { useNavigate } from "react-router-dom";
 
 export function NavBar({ setResults }) {
-  const [value, setValue] = useState();
+  const [value, setValue] = useState("/");
+  const theme = useTheme();
+  const isMatch = useMediaQuery(theme.breakpoints.down("sm"));
+  const navigate = useNavigate();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
         <Toolbar>
-          <Tabs
-            textColor='inherit'
-            value={value}
-            onChange={(e, value) => setValue(value)}
-          >
-            <Tab label='Movies' />
-            <Tab label='TV Shows' />
-            <Tab label='People' />
-          </Tabs>
+          {isMatch ? (
+            <DrawerMenu />
+          ) : (
+            <Tabs
+              textColor='inherit'
+              value={value}
+              onChange={(e, value) => {
+                setValue(value);
+                navigate(`${value}`);
+              }}
+            >
+              <Tab value='/' label='Home' />
+              <Tab value='/movies' label='Movies' />
+              <Tab value='/tv' label='TV Shows' />
+              <Tab value='/people' label='People' />
+            </Tabs>
+          )}
           <Box
             sx={{ marginLeft: "auto" }}
             display={{ xs: "none", sm: "block" }}
